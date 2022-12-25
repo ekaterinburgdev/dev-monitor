4b979ae7e514a80ef657f698ad8124f9d288c38a
Anton Akhatov <anton@gcor.ru>
2022-12-25 23:56:49 +0000

    add contributions statistics

--- components/Activity/Activity.jsx
+++ components/Activity/Activity.jsx
@@ -3,33 +3,59 @@ import uniq from "lodash/uniq";
 import styles from "./Activity.module.css";
 import { Loading } from "../Loading/Loading";
 
-let monthFormatter = new Intl.DateTimeFormat("en", {
+const DAY = 1000 * 60 * 60 * 24;
+const SHOW_WEEKS = { S: 20, M: 38, L: 56 };
+const SHOW_WEEKS_BREAKPOINTS = { S: 700, L: 1600 };
+
+const monthFormatter = new Intl.DateTimeFormat("en", {
   month: "short",
 });
 
+const commitDateFormatter = new Intl.DateTimeFormat("en", {
+  month: "short",
+  day: "numeric",
+});
+
+const descriptionFormatter = ({ index, day, week }) => {
+  const date = new Date(week * 1000 + index * DAY);
+
+  return {
+    short: `${day}, ${commitDateFormatter.format(date)}`,
+    full: `${day} contribution${
+      day > 1 ? `s` : ""
+    } on ${commitDateFormatter.format(date)}`
+  };
+};
+
 export function Activity({ activity }) {
-  const [limit, setLimit] = useState(1000);
+  const [limit, setLimit] = useState(SHOW_WEEKS.L);
 
   const { months, days } = useMemo(() => {
     if (!activity) return {};
 
     const year = activity.slice(-limit);
     const days = year
-      .reduce((all, item) => all.concat(item.days), [])
-      .map((value) => {
-        if (value > 6) return 3;
-        if (value > 3) return 2;
-        if (value > 0) return 1;
+      .reduce(
+        (all, item) =>
+          all.concat(
+            item.days.map((d, index) => ({ day: d, week: item.week, index }))
+          ),
+        []
+      )
+      .map((item) => {
+        const result = { ...item, description: descriptionFormatter(item) };
+        if (item.day > 6) return { ...result, level: 3 };
+        if (item.day > 3) return { ...result, level: 2 };
+        if (item.day > 0) return { ...result, level: 1 };
 
-        return 0;
+        return { ...result, level: 0 };
       });
 
     const months = uniq(
       year
         .map((w) => {
-          if (w.week * 1000) {
-            return new Date(w.week * 1000);
-          }
+          const timestamp = w.week * 1000;
+          if (timestamp) return new Date(timestamp);
           return false;
         })
         .filter(Boolean)
@@ -41,12 +67,12 @@ export function Activity({ activity }) {
 
   useEffect(() => {
     const onResize = () => {
-      if (window.innerWidth < 700) {
-        setLimit(20);
-      } else if (window.innerWidth < 1600) {
-        setLimit(38);
+      if (window.innerWidth < SHOW_WEEKS_BREAKPOINTS.S) {
+        setLimit(SHOW_WEEKS.S);
+      } else if (window.innerWidth < SHOW_WEEKS_BREAKPOINTS.L) {
+        setLimit(SHOW_WEEKS.M);
       } else {
-        setLimit(56);
+        setLimit(SHOW_WEEKS.L);
       }
     };
 
@@ -63,15 +89,21 @@ export function Activity({ activity }) {
   }
 
   return (
-    <div className={styles.graph}>
+    <div className={styles.graph} aria-hidden>
       <ul className={styles.months}>
         {months.map((m) => (
           <li key={m}>{m}</li>
         ))}
       </ul>
       <ul className={styles.squares}>
-        {days.map((value, i) => (
-          <li key={i} data-level={value} />
+        {days.map(({ description, level }, i) => (
+          <li
+            className={styles.square}
+            key={i}
+            data-level={level}
+            data-description-short={description.short}
+            data-description-full={description.full}
+          />
         ))}
       </ul>
     </div>
