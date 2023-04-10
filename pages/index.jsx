762440a8818375dfc6e78c93461e8ffef61756d8
Alexander Chabin <nibach@ya.ru>
2023-04-10 17:32:35 +0500

    Add opening links in a new window in the widget

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,18 @@
+import Head from 'next/head'
 import { Dashboard } from "../components/Dashboard/Dashboard";
 import getAllRepoStats from "../github-api/get-all-repo-stats";
 
 function Home({ projectsData, isWidgetVersion = false }) {
   return (
+    <>
+    <Head>
+      <base target='_blank'></base>
+    </Head>
     <Dashboard
       projectsData={projectsData}
       isWidgetVersion={isWidgetVersion}
     />
+    </>
   )
 }
 
